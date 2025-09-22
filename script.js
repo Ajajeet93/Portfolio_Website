1b8bbfb225bedfefbdee583804dc1a24108adeb8
Ajeet Yadav <149400167+Ajajeet93@users.noreply.github.com>
2025-09-22 13:03:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navLinks.classList.toggle("active");
 });
+
+const form_info=document.getElementById("show_msg")
+form_info.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    const user_name=document.getElementById("user_name").value;
+    const displayMsg = document.getElementById("display_msg");
+    const msg_id=document.getElementById("msg1")
+    displayMsg.classList.add("show");
+    msg_id.innerHTML=`Hello ${user_name}! Thank you for contacting me. <br> I will get back to You soon.`
+    setTimeout(() =>{
+        displayMsg.classList.remove("show");
+        msg_id.textContent=''
+    }, 5000)
+    form_info.reset();
+})
